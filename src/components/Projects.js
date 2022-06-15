767ee71f0f9b4e663868715f5a2118da0518cce5
andrew-merz <merz.school@gmail.com>
2022-06-15 19:28:51 -0400

    more styling added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,31 @@
 import { Typography, Card } from "@mui/material";
-
+import "../projects.css";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 
 function Projects() {
   return (
-    <div>
+    <div className="cards">
       <Card sx={{ maxWidth: 285 }}>
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             Project 1
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+      <Card sx={{ maxWidth: 285 }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Project 2
           </Typography>
           <Typography variant="body2" color="text.secondary">
             Lizards are a widespread group of squamate reptiles, with over 6,000
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { Typography, Card } from "@mui/material";
+import { Typography } from "@mui/material";
 import Projects from "../components/Projects";
 
 function Home() {
@@ -6,14 +6,16 @@ function Home() {
     <div className="App">
       <h1>Andrew Merz</h1>
       <h4>Portfolio</h4>
-      <Typography align="center">
-        I am a full stack software engineer based in the greater New York City
-        area with a background in business and finance. I am currently enrolled
-        in General Assembly's full time Software Engineering Immersive program
-        where I am learning programming and computer science fundamentals. Upon
-        graduation I am looking to embark on my journey as a software engineer
-        working on cutting edge software and technology.
-      </Typography>
+      <div className="summary">
+        <Typography align="center">
+          I am a full stack software engineer based in the greater New York City
+          area with a background in business and finance. I am currently
+          enrolled in General Assembly's full time Software Engineering
+          Immersive program where I am learning programming and computer science
+          fundamentals. Upon graduation I am looking to embark on my journey as
+          a software engineer working on cutting edge software and technology.
+        </Typography>
+      </div>
       <Projects />
     </div>
   );
